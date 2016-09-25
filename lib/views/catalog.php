25ce39ee4e98b448e23b384c7370e4abd15dbffd
Tonya <hellofromtonya@thewpdc.com>
2016-09-24 20:11:27 -0500

    styling

--- lib/views/catalog.php
+++ lib/views/catalog.php
@@ -1,12 +1,10 @@
 <section class="catalog">
-	<div class="wrap">
-		<ul class="grid__container">
-		<?php foreach( $catalog_items as $term ) :
-			$term_link = get_term_link( $term, 'catalog' ); ?>
-			<li class="grid__block">
-				<a href="<?php echo esc_url( $term_link ); ?>"><?php esc_html_e( $term->name ); ?> ( <?php echo (int) $term->count; ?> )</a>
-			</li>
-		<?php endforeach; ?>
-		</ul>
-	</div>
+	<ul>
+	<?php foreach( $catalog_items as $term ) :
+		$term_link = get_term_link( $term, 'catalog' ); ?>
+		<li>
+			<a href="<?php echo esc_url( $term_link ); ?>"><?php esc_html_e( $term->name ); ?> ( <?php echo (int) $term->count; ?> )</a>
+		</li>
+	<?php endforeach; ?>
+	</ul>
 </section>
