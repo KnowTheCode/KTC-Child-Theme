a3bac0367c0a3918f99ced843f60bf361d280a5d
Tonya <hellofromtonya@thewpdc.com>
2016-10-17 19:52:08 -0500

    setup

--- lib/setup.php
+++ lib/setup.php
@@ -10,7 +10,7 @@
  */
 namespace KnowTheCode;
 
-add_action( 'genesis_setup', __NAMESPACE__ . '\setup', 15 );
+add_action( 'genesis_setup', __NAMESPACE__ . '\setup_child_theme', 15 );
 /**
  * Theme setup.
  *
@@ -19,17 +19,33 @@ add_action( 'genesis_setup', __NAMESPACE__ . '\setup', 15 );
  *
  * @since 1.0.0
  */
-function setup() {
+function setup_child_theme() {
+	adds_theme_supports();
+	unregister_layouts();
 
-	add_theme_support( 'html5', array( 'caption', 'comment-form', 'comment-list', 'gallery', 'search-form' ) );
+	add_filter( 'edit_post_link', '__return_empty_string' );
 
-	add_theme_support( 'genesis-responsive-viewport' );
+	unregister_genesis_callbacks();
+}
 
-	add_theme_support( 'genesis-footer-widgets', 4 );
-
-	add_theme_support(
-		'genesis-structural-wraps',
-		array(
+/**
+ * Adds theme supports to the site.
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+function adds_theme_supports() {
+	$config = array(
+		'html5'                       => array(
+			'caption',
+			'comment-form',
+			'comment-list',
+			'search-form'
+		),
+		'genesis-responsive-viewport' => null,
+		'genesis-footer-widgets'      => 4,
+		'genesis-structural-wraps'    => array(
 			'footer',
 			'footer-widgets',
 			'header',
@@ -37,32 +53,48 @@ function setup() {
 			'site-inner',
 			'site-tagline',
 			'partners__footer',
-		)
+		),
+		'genesis-menus'               => array(
+			'primary' => __( 'Primary Navigation Menu', CHILD_TEXT_DOMAIN ),
+			'footer'  => __( 'Footer Navigation Menu', CHILD_TEXT_DOMAIN ),
+		),
 	);
-
-	add_theme_support(
-		'genesis-menus',
-		array(
-			'primary' => __( 'Primary Navigation Menu', 'ktc' ),
-			'footer'  => __( 'Footer Navigation Menu', 'ktc' ),
-		)
-	);
-
-	genesis_unregister_layout( 'sidebar-content' );
-	genesis_unregister_layout( 'content-sidebar' );
-	genesis_unregister_layout( 'content-sidebar-sidebar' );
-	genesis_unregister_layout( 'sidebar-content-sidebar' );
-	genesis_unregister_layout( 'sidebar-sidebar-content' );
-	
-	// temporary fix for Genesis bug 06.22.2016
-	genesis_set_default_layout( 'full-width-content' );
-
-	add_filter( 'edit_post_link', '__return_empty_string' );
-
-	unregister_genesis_defaults();
+	foreach ( $config as $feature => $args ) {
+		add_theme_support( $feature, $args );
+	}
 }
 
-function unregister_genesis_defaults() {
+/**
+ * Unregister the Genesis Layouts.
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+function unregister_layouts() {
+	$layouts = array(
+		'sidebar-content',
+		'content-sidebar',
+		'content-sidebar-sidebar',
+		'sidebar-content-sidebar',
+		'sidebar-sidebar-content',
+	);
+	foreach( $layouts  as $layout ) {
+		genesis_unregister_layout( $layout );
+	}
+
+	// temporary fix for Genesis bug 06.22.2016
+	genesis_set_default_layout( 'full-width-content' );
+}
+
+/**
+ * Unregister Genesis callbacks.  We do this here because the child theme loads before Genesis.
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+function unregister_genesis_callbacks() {
 	unregister_header_events();
 	unregister_nav_events();
 	unregister_post_events();
@@ -70,7 +102,59 @@ function unregister_genesis_defaults() {
 	unregister_footer_events();
 }
 
+add_filter( 'genesis_theme_settings_defaults', __NAMESPACE__ . '\set_theme_settings_defaults' );
+/**
+ * Set theme settings defaults.
+ *
+ * @since 1.0.0
+ *
+ * @param array $defaults
+ *
+ * @return array
+ */
+function set_theme_settings_defaults( array $defaults ) {
+	$config   = get_theme_settings_defaults();
+	$defaults = wp_parse_args( $config, $defaults );
+
+	return $defaults;
+}
+
+add_action( 'after_switch_theme', __NAMESPACE__ . '\update_theme_settings_defaults' );
+/**
+ * Sets the theme setting defaults.
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+function update_theme_settings_defaults() {
+	$config = get_theme_settings_defaults();
+	if ( function_exists( 'genesis_update_settings' ) ) {
+		genesis_update_settings( $config );
+	}
+	update_option( 'posts_per_page', $config['blog_cat_num'] );
+}
+
+/**
+ * Get the theme settings defaults.
+ *
+ * @since 1.0.0
+ *
+ * @return array
+ */
+function get_theme_settings_defaults() {
+	return array(
+		'blog_cat_num'              => 10,
+		'content_archive'           => 'full',
+		'content_archive_limit'     => 250,
+		'content_archive_thumbnail' => 0,
+		'posts_nav'                 => 'numeric',
+		'site_layout'               => 'full-width-content',
+	);
+}
+
 add_filter( 'theme_page_templates', __NAMESPACE__ . '\remove_genesis_page_templates' );
+
 /**
  * Remove Genesis Blog page template.
  *
@@ -83,7 +167,6 @@ function remove_genesis_page_templates( $page_templates ) {
 
 	return $page_templates;
 }
-
 
 if ( function_exists( 'fulcrum_load_login_form_styling' ) ) {
 	/**
