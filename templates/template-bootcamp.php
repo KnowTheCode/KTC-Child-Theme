d1e7ed4975bc8cfa96c00db51d177180d0aef33c
hellofromtonya <hellofromtonya@uptechlabs.io>
2017-04-12 20:56:00 -0500

    bootcamp template + styles

--- templates/template-bootcamp.php
+++ templates/template-bootcamp.php
@@ -1,15 +1,16 @@
 <?php
-
 /**
- * Front page template
+ * Bootcamp Teaser
  *
- * @package     KnowTheCode\Front_Page
- * @since       1.6.1
+ * Template Name: Bootcamp Teaser
+ *
+ * @package     KnowTheCode\BootcampTeaser
+ * @since       1.0.0
  * @author      hellofromTonya
  * @link        https://UpTechLabs.io
- * @license     GNU General Public License 2.0+ and MIT Licence (MIT)
+ * @license     GNU General Public License 2.0+
  */
-namespace KnowTheCode\FrontPage;
+namespace KnowTheCode\BootcampTeaser;
 
 remove_all_actions( 'genesis_entry_header' );
 remove_all_actions( 'genesis_entry_footer' );
@@ -18,7 +19,7 @@ remove_action( 'genesis_before_header', 'KnowTheCode\Structure\render_utility_ba
 remove_action( 'genesis_after_header', 'KnowTheCode\Structure\render_sub_nav', 12 );
 remove_action( 'genesis_after_header', 'KnowTheCode\Structure\render_main_nav' );
 
-add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_front_page_assets' );
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_assets' );
 /**
  * Enqueue the specific assets for this template, as there's no load it on every
  * single page.
@@ -27,23 +28,23 @@ add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_front_page_assets' )
  *
  * @return void
  */
-function enqueue_front_page_assets() {
-	wp_enqueue_style( 'ktc_front_page_css', CHILD_URL . '/assets/dist/css/front-page.min.css', array(), '1.6.5' );
+function enqueue_assets() {
+	wp_enqueue_style( 'ktc_bootcamp_css', CHILD_URL . '/assets/dist/css/bootcamp-teaser.min.css', array(), '1.0.0' );
 	wp_enqueue_script( 'ktc_front_page_js', CHILD_URL . '/assets/dist/js/jquery.front-page.min.js', array('jquery'), '1.6.5', true );
 }
 
-add_action( 'genesis_header', __NAMESPACE__ . '\render_front_page_main_nav', 11 );
+add_action( 'genesis_header', __NAMESPACE__ . '\render_main_nav', 11 );
 /**
- * Render navigation.i
+ * Render navigation.
  *
  * @since 1.3.0
  *
  * @return void
  */
-function render_front_page_main_nav() {
+function render_main_nav() {
 	$user_is_logged_in = is_user_logged_in();
 
-	require_once( __DIR__ . '/lib/structure/views/front-page-main-nav.php' );
+	require_once( CHILD_THEME_DIR . '/lib/structure/views/front-page-main-nav.php' );
 }
 
 genesis();
